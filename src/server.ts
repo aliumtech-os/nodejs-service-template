fb3f90708ff574c6de93558b80eea7cfed7cc5f3
“Ankit <roy.ankitin2025@gmail.com>
2025-12-11 12:31:36 +0530

    test aliasing

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import { testUtil } from '@utils/test';
+
+console.log('TypeScript is working!');
+console.log(testUtil());
+
+
+// Test path alias (once you create the file)
+// import { logger } from '@utils/logger';
